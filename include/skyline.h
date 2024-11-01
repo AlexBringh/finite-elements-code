eed1c4c36cc981671ee65649bc43dffa1044dce8
AlexBringh <115021574+AlexBringh@users.noreply.github.com>
2024-11-01 13:29:34 +0100

    Skyline header, function prototypes

--- include/skyline.h
+++ include/skyline.h
@@ -4,4 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+skylineMatrix* initSkylineMatrix (int n);
+int addSkylineElement (skylineMatrix* matrix, int m, int n, double val);
+int getSkylineElement (skylineMatrix* matrix, int m, int n);
+int updateSkylineElement (skylineMatrix* matrix, int m, int n, double val);
+
 #endif
