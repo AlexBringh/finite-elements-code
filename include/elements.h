9483e008c35ac9746e8dac04ff0485ceafbe9234
AlexBringh <115021574+AlexBringh@users.noreply.github.com>
2025-05-15 14:31:35 +0200

    Save

--- include/elements.h
+++ include/elements.h
@@ -4,15 +4,16 @@
 typedef struct {
 
     int id; // Element id
+    int gp; // Number of Gauss Points
     int nnodes; // Number of nodes
     int dof; // Degrees of freedom per node
-    int *connectivity; // node connectivity numbering
-    int *coords; // Coordinates of connected nodes from local numbering 1 to nnodes.
-    int gp; // Number of Gauss Points
+    int *connectivity; // Node connectivity numbering
+    int *coords; // Global coordinates of connected nodes.
 
+    // Accumulated stress and plastic strain.
+    double sigma;
     double epsilon_p;
     double epsilon_bar_p;
-    double sigma;
 
 } quadElement;
 
--- src/main.c
+++ src/main.c
@@ -19,6 +19,60 @@ int main (char *args)
     /*
         The entry point of the program.
     */
+    /*    
+    Step 1: Import and initialize Data
+        - Load input data: Node coords, Element connectivity, Boundary conditions, Applied load vectors, Material properties (E, v, sigma_yield_0, H)
+        - Compute and store shape functions and its derivatives in the reference space (xi, eta) (static values)
+        - Define Gauss Point locations and weights for numerical integration (static values)
+
+    Step 2: Initialize Gauss Point internal variables
+        - For each element and Gauss Point:
+            - epsilon_p = 0
+            - epsilon_bar_p = 0
+            - sigma = 0
+        - These variables will be updated as plasticity accumulates per load step
+    
+    From here, everything is within the iterator, k
+    Step 3: Newton-Raphson loop starts
+        - Apply load increment (f_ext + delta f_ext)
+        - Get the initial displacement "guess" (not actually a guess. First is the initial value, often u0 = 0, and from each step it will be incremented from the solution of the system).
+
+    Step 4: Loop over elements & Gauss points
+        - Extract the nodal displacements for the element from u, -> u_e
+        - Compute the Jacobian and B-matrix
+        - Compute the strain as epsilon = B * u_e
+        - Compute the trial stress as sigma_trial = D_e (epsilon - epsilon_p)
+        - From the trial stress, calculate the trial deviatoric stress and the von Mises equivalent stress
+        - Check the trial stress against the yield function f = sigma_eq - sigma_y, where sigma_y = sigma_y0 + H * epsilon_bar_p
+        - If f > 0 -> Plasticity (comput delta gamma, n, and D_ep), if f <= 0 -> elasticity (use D_e)
+            - if f > 0 -> Compute delta gamma = f_yield / (3 G + H)
+            - G = E / (2(1+v))
+            - Correct stress on Gauss point as sigma = sigma_trial - 2 G * delta gamma * n
+    
+    Step 5: Compute Local Element Quantities (could be included in step 4 since it is still in each element loop)
+        - Compute K_e = sum(gp) Btrans D(e / ep determined in step 4) B * detJ * weight(gp)
+        - Compute f_int_e = sum(gp) Btrans * sigma * detJ * weight(gp)
+    
+    Step 6: Assemble global system
+        - Assemble global stiffness matrix, K
+        - Assemble global internal force vector
+
+    Step 7: Solve system and update displacement
+        - r^(k) = f_ext^(k) - f_int^(k)
+        - K^(k) * delta u^(k) = r^(k)
+        - Increment u^(k+1) = u^(k) + delta u^(k)
+
+    Step 8: Convergence check for the current step
+        - Check that abs(r^(k)) < tolerance or abs(delta u^(k)) is small
+        - if converged ->
+            - Commit values for epsilon_p, epsilon_bar_p and sigma for each Gauss Point
+            - Add a load increment and go to the next Newton-Raphson step. (Go to step 3 and add to f_ext).
+        - else 
+            - Go back to step 3 but do not add a load increment if not converged!
+    
+    Step 9: Once the entire load is applied, and the system has converged
+        - Run post-processing to extract results in useful formats.
+    */
     printf("Running Finite Elements Simulation on Plasticity Equations! \n");
 
     // Initialize -> Material Properties -> Stress and Strain Variables -> Initial Conditions and Read Input Data (Geometry, Boundary Conditions & Loading)
@@ -26,7 +80,17 @@ int main (char *args)
     
     // Constant values for the analysis
     int gp = 4; // Gauss Points per element
-    int DOF = 2; // Degrees of freedom per node / Gauss Point
+    int nelements = 100; // Number of elements in the mesh.
+    int nnodes = 200; // Number of nodes in the mesh.
+    int DOF = 2; // Degrees of freedom per node
+    int Km = nnodes * DOF; // Number of rows / columns of the global stiffness matrix.
+
+    // Allocate memory for the 'element' structs.
+    quadElement *element = malloc(nelements * sizeof(quadElement));
+
+    // Allocate memory for the global displacement vector.
+    double *u = malloc(nnodes * DOF * sizeof(double));
+    double *du = malloc(nnodes * DOF * sizeof(double));
 
     // Get the shape functions for 2D Quad elements
     int j_m = 2; // Representing the size, m, in a m*m Jacobian matrix.
@@ -68,7 +132,6 @@ int main (char *args)
 
     }
 
-    int nelements = 100; // PLACEHOLDER VALUE.
     for (int i = 0; i < nelements; i++) // Loop through each element and find the local Jacobian matrix, its determinant, the B-matrix, the B-transpose, the D-matrix, and calculate the element stiffness matrix. 
     {
         double *xi; // TODO: Make logic for extracting nodes coordinates into xi and yi for each element.
@@ -81,7 +144,10 @@ int main (char *args)
 
     }
 
+    // Post-processing
+
     // Freeing some variables from memory now that they are no longer needed.
+    free(element); // Element structs
     free(Ni); // Local shape functions.
     free(NiPxi); // Partial derivatives of local shape functions, of Xi (greek letter).
     free(NiPeta); // Åarial derivatives of local shape functions, of Eta.
@@ -135,6 +201,7 @@ int main (char *args)
         - r^(k) = f_ext^(k) - f_int^(k)
         - K^(k) * delta u^(k) = r^(k)
         - Increment u^(k+1) = u^(k) + delta u^(k)
+
     Step 8: Convergence check for the current step
         - Check that abs(r^(k)) < tolerance or abs(delta u^(k)) is small
         - if converged ->
@@ -142,6 +209,9 @@ int main (char *args)
             - Add a load increment and go to the next Newton-Raphson step. (Go to step 3 and add to f_ext).
         - else 
             - Go back to step 3 but do not add a load increment if not converged!
+    
+    Step 9: Once the entire load is applied, and the system has converged
+        - Run post-processing to extra results in useful formats.
     */
 
     return 0;
