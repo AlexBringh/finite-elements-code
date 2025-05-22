24edc0eaf85613b26898de18c3625b18a15b65e0
AlexBringh <115021574+AlexBringh@users.noreply.github.com>
2025-05-22 22:49:07 +0200

    Save

--- src/math/matrixArithmetic.c
+++ src/math/matrixArithmetic.c
@@ -11,14 +11,18 @@ int matrixSum (double *A, double *B, double *C, int m, int n)
     /*
         Sums the values of the A-matrix and the B-matrix, and stores the result in the given C-matrix.
         A and B must be equal m x n (row x column) matrices, or the program will cause errors.
+
+        Inputs:
+        double *A -> Pointer to A matrix (m x n)
+        double *B -> Pointer to B matrix (m x n)
+        double *C -> Pointer to C matrix (m x n) (results are stored here)
+        int m -> size m
+        int n -> size n
     */
 
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < m * n; i++)
     {
-        for (int j = 0; j < n; j++)
-        {
-            *(C + i * n + j) = *(A + i*n + j) + *(A + i*n + j);
-        }
+        *(C + i) = *(A + i) + *(B + i);
     }
 
     return 0;
@@ -29,14 +33,18 @@ int matrixSubtract (double *A, double *B, double *C, int m, int n)
     /*
         Subtracts the values of the A-matrix using the B-matrix and stores the result in the given C-matrix.
         A and B must be equal m x n (row x column) matrices, or the program will cause errors.
+
+        Inputs:
+        double *A -> Pointer to A matrix (m x n)
+        double *B -> Pointer to B matrix (m x n)
+        double *C -> Pointer to C matrix (m x n) (results are stored here)
+        int m -> size m
+        int n -> size n
     */
 
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < m * n; i++)
     {
-        for (int j = 0; j < n; j++)
-        {
-            *(C + i * n + j) = *(A + i*n + j) - *(A + i*n + j);
-        }
+        *(C + i) = *(A + i) - *(B + i);
     }
 
     return 0;
@@ -47,6 +55,14 @@ int matrixMultiply (double *A, double *B, double *C, int m, int n, int h)
     /*
         Multiplies 2 matrices together (A x B) and stores the result in the matrix C as a m x h (row x column) matrix.
         A must be a m x n matrix (row x column), whilst B must be a n x h (row x column) matrix.
+
+        Inputs:
+        double *A -> Pointer to A matrix (m x n)
+        double *B -> Pointer to B matrix (n x h)
+        double *C -> Pointer to C matrix (m x h) (results are stored here)
+        int m -> size m
+        int n -> size n
+        int h -> size h
     */
 
     for (int i = 0; i < m; i++)
